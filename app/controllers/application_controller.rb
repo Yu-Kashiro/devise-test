7300f2649fa08ec1e66d3209bf7370e8e64ebdce
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-08-04 14:40:36 +0900

    Deviseのカスタムコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit({ roles: [] },
+      :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
